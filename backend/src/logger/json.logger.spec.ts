17436d8eebe28882144eb1f2b6b99ce4de518e27
Roman Kirillov <roman.kirillov.06@mail.ru>
2026-08-26 11:26:35 +0300

    feat(add):  loggers and docker files

---
+++ backend/src/logger/json.logger.spec.ts
@@ -0,0 +1,55 @@
+import { JsonLogger } from './json.logger';
+
+describe('JsonLogger tests', () => {
+  let jsonLogger: JsonLogger;
+
+  beforeEach(() => {
+    jsonLogger = new JsonLogger();
+  });
+
+  afterEach(() => {
+    jest.restoreAllMocks();
+  });
+
+  it('.log() should be log in json format', () => {
+    const mockFunction = jest
+      .spyOn(console, 'log')
+      .mockImplementation(() => {});
+    const message = 'json log';
+    jsonLogger.log(message);
+    const result = JSON.stringify({
+      level: 'log',
+      message: message,
+      optionalParams: [],
+    });
+    expect(mockFunction).toHaveBeenCalledWith(result);
+  });
+
+  it('.error() should be error in json format', () => {
+    const mockFunction = jest
+      .spyOn(console, 'error')
+      .mockImplementation(() => {});
+    const message = 'json error';
+    jsonLogger.error(message);
+    const result = JSON.stringify({
+      level: 'error',
+      message: message,
+      optionalParams: [],
+    });
+    expect(mockFunction).toHaveBeenCalledWith(result);
+  });
+
+  it('.warn() should be warn in json format', () => {
+    const mockFunction = jest
+      .spyOn(console, 'warn')
+      .mockImplementation(() => {});
+    const message = 'json warn';
+    jsonLogger.warn(message);
+    const result = JSON.stringify({
+      level: 'warn',
+      message: message,
+      optionalParams: [],
+    });
+    expect(mockFunction).toHaveBeenCalledWith(result);
+  });
+});
